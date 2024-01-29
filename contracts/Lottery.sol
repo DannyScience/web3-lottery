67d685857f944d4dcf46ccaa9a1b477025756b36
DannyScience <s.denis1125@gmail.com>
2024-01-28 22:49:22 -0800

    main contract update

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,6 +7,13 @@ contract Lottery {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface public ethUsdPriceFeed;
+    enum LOTTERY_STATE{
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+
 
     constructor(address _priceFeedAddress) {
         usdEntryFee = 50 * 10 ** 18;
@@ -14,7 +21,26 @@ contract Lottery {
 
     }
 
-    function enter() public payable {}
-    function getEntranceFee() public view returns (uint256) {}
+    function enter() public payable {
+        require(lottery_state == LOTTERY_STATE.OPEN, "Lottery is not active");
+        require(msg.value >= calculateEntranceFee(), "Not enough ETH");
+        players.push(payable(msg.sender));
+    }
 
+    function calculateEntranceFee() public view returns (uint256) {
+        ( , uint price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 ethEntryFee = (usdEntryFee / (price * 10 ** 10)) * 10 ** 18;
+    return ethEntryFee;
+    }
+
+    function startLottery() public {
+        require(lottery_state == LOTTERY_STATE.CLOSED);
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public {
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+
+    }
 }
